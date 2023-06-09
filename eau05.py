acbd7665de330a5ed8c27e0a24d51740071ecd79
Yoann Alves <yoannalves7@gmail.com>
2023-06-09 17:36:17 +0200

    structure de base

--- eau05.py
+++ eau05.py
@@ -1,19 +1,36 @@
 ############### String dans string ###############
 
+import sys
+
 ### Function ###
 
+def include_string(a: str, b: str) -> bool:
+    ...
 
+
+def format_result_include(a: str, b: str) -> None:
+    ...
+
+
+def have_not_correct_number_of_arguments() -> None:
+
+    if len(sys.argv) != 3:
+        print("Error")
+        exit()
+
+
+def is_not_contain_letters(a: str, b:str) -> bool:
+    ...
 
 
 ### Error ###
 
-
+have_not_correct_number_of_arguments()
 
 
 ### Parsing ###
 
-
-
+strings = sys.argv[1:3]
 
 ### Problem solving ###
 
