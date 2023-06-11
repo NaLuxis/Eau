30f1cff31c3452cfb3aa82465857db300fafb5d6
Yoann Alves <yoannalves7@gmail.com>
2023-06-11 13:01:08 +0200

    Error good

--- eau05.py
+++ eau05.py
@@ -2,6 +2,8 @@
 
 import sys
 
+import re
+
 ### Function ###
 
 def include_string(a: str, b: str) -> bool:
@@ -20,17 +22,33 @@ def have_not_correct_number_of_arguments() -> None:
 
 
 def is_not_contain_letters(a: str, b:str) -> bool:
-    ...
+    
+    match = re.search("[a-zA-Z]+", a and b)
+
+    if match:
+        return True
+    
+
+def error_arg(a: str, b: str) -> None:
+
+    if is_not_contain_letters(a, b):
+        return a and b
+    else:
+        print("Error")
+        exit()
 
 
 ### Error ###
 
 have_not_correct_number_of_arguments()
 
+error_arg(sys.argv[1], sys.argv[2])
 
 ### Parsing ###
 
-strings = sys.argv[1:3]
+string_a = sys.argv[1]
+
+string_b = sys.argv[2]
 
 ### Problem solving ###
 
