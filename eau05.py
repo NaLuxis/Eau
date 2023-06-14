048b1e639500e2c5641c261afd7f5943d9bd4e98
Yoann Alves <yoannalves7@gmail.com>
2023-06-14 08:16:51 +0200

    good

--- eau05.py
+++ eau05.py
@@ -7,11 +7,21 @@ import re
 ### Function ###
 
 def include_string(a: str, b: str) -> bool:
-    ...
+
+    if a.find(b) == -1:
+        return False
+    else:
+        return True
 
 
 def format_result_include(a: str, b: str) -> None:
-    ...
+    
+    if include_string(a, b):
+        print("true")
+        exit()
+    else:
+        print("false")
+        exit()
 
 
 def have_not_correct_number_of_arguments() -> None:
@@ -27,13 +37,23 @@ def is_not_contain_letters(a: str, b:str) -> bool:
 
     if match:
         return True
-    
+
+
+def is_not_correct_length(a: str, b: str) -> bool:
+
+    if len(a) < len(b):
+        return True
+
 
 def error_arg(a: str, b: str) -> None:
 
     if is_not_contain_letters(a, b):
-        return a and b
+        pass
     else:
+        print("Error")
+        exit()
+
+    if is_not_correct_length(a, b):
         print("Error")
         exit()
 
@@ -52,7 +72,8 @@ string_b = sys.argv[2]
 
 ### Problem solving ###
 
-
-
+final_result = format_result_include(string_a, string_b)
 
 ### Result ###
+
+print(final_result)
