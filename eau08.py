3d82f88cbf79e6d743ca9086acdd6b738896e2f7
Yoann <yoannalves7@gmail.com>
2023-06-28 16:48:55 +0200

    ok

--- eau08.py
+++ eau08.py
@@ -1,23 +1,40 @@
 ############### Chiffres only ###############
 
+import sys, re
+
 ### Function ###
 
+def is_number(arg: any) -> bool:
 
+    for char in arg:
+
+        if not re.match(r"\d", char):
+            return False
+    
+    else:
+        return True
+
+
+def incorrect_argument_count() -> None:
+
+    if len(sys.argv) != 2:
+        print("Error")
+        exit()
 
 
 ### Error ###
 
-
+incorrect_argument_count()
 
 
 ### Parsing ###
 
-
-
+number_arg = sys.argv[1]
 
 ### Problem solving ###
 
-
-
+arg_is_numbber = is_number(number_arg)
 
 ### Result ###
+
+print(arg_is_numbber)
