7747c751d089ef4bc0950e6cadf373a9e93e53e6
Yoann <yoannalves7@gmail.com>
2023-06-28 16:11:40 +0200

    ok

--- eau07.py
+++ eau07.py
@@ -5,7 +5,25 @@ import sys, re
 ### Function ###
 
 def capitalize_first_letter(arg: str) -> str:
-    ...
+
+    lower_arg = arg.lower()
+
+    words_list = lower_arg.split(" ")
+
+    final_list = []
+
+    for word in words_list:
+        new_word = []
+        capitalized_letter = word[0].upper()
+        new_word.append(capitalized_letter)
+        new_word.append(word[1:])
+        final_word = "".join(new_word)
+        final_list.append(final_word)
+
+    final_sentence = " ".join(final_list)
+    
+    return final_sentence
+
 
 
 def incorrect_argument_count() -> None:
@@ -15,9 +33,9 @@ def incorrect_argument_count() -> None:
         exit()
 
 
-def has_no_letters(a: str) -> bool:
+def has_no_letters(arg: str) -> bool:
     
-    if re.search(r"[a-zA-Z]+", a):
+    if re.search(r"[a-zA-Z]+", arg):
         return True
 
 
@@ -38,12 +56,12 @@ error_argument(sys.argv[1])
 
 ### Parsing ###
 
-
-
+string_arg = sys.argv[1]
 
 ### Problem solving ###
 
-
-
+capitalize = capitalize_first_letter(string_arg)
 
 ### Result ###
+
+print(capitalize)
